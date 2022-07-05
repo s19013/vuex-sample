0d3435b9936a068017c8a11329e8bc47f41d8a3e
知念英弥 <s19013@std.it-college.ac.jp>
2022-07-05 11:10:55 +0900

    vue3用の書き方に変更

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,6 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import { createStore } from 'vuex'
 
-Vue.use(Vuex)
-
-export default new Vuex.Store({
+export default createStore({
 	state: {
 		message: 'Hello Vuex'
 	},
@@ -14,3 +11,4 @@ export default new Vuex.Store({
   modules: {
   }
 })
+
