5371dccbdb4134b267e507497ec6e9518cc54d1a
知念英弥 <s19013@std.it-college.ac.jp>
2022-07-05 12:03:47 +0900

    userListコンポーネント設置

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,26 @@
 <template>
 	<div id="app">
 		<h1>{{$store.state.message}}</h1>
+		<UserList></UserList>
 	</div>
 </template>
 
 <script>
-
+import  UserList  from "./components/UserList.vue";
 
 export default {
   name: 'App',
   components: {
+		UserList
   },
   data() {
-	return {
+		return {
 
-	}
+		}
   },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
