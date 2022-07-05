262222b989bde9a6f8354fd13151aef89b34b86b
知念英弥 <s19013@std.it-college.ac.jp>
2022-07-05 12:01:32 +0900

    ユーザーの配列

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,12 @@ import { createStore } from 'vuex'
 
 export default createStore({
 	state: {
-		message: 'Hello Vuex'
+		message: 'Hello Vuex',
+		users:[
+			{name: 'John', email:'john@example.com', age:22},
+			{name: 'Merry', email: 'merry@facebook.com',age:33},
+			{name: 'Ken', email: 'ken@amazon.com',age:29}
+		],
 	},
 	mutations: {
   },
