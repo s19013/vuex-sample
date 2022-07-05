1184451191f224cdc35c3473686559d9ecea5345
知念英弥 <s19013@std.it-college.ac.jp>
2022-07-05 12:28:49 +0900

    vuexからgettersの変数を取得?

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,15 +1,25 @@
 <template>
-  <ul>
-		<li v-for="user in users"  v-bind:key="user.name">
-			{{ user.name }} ({{ user.email }})
-		</li>
-  </ul>
+	<div>
+		<p>全員</p>
+		<ul>
+			<li v-for="user in users"  v-bind:key="user.name">
+				{{ user.name }} ({{ user.email }})
+			</li>
+		</ul>
+		<p>30以下</p>
+		<ul>
+			<li v-for="user in teenAger"  v-bind:key="user.name">
+				{{ user.name }} ({{ user.email }})
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
 export default {
 computed:{
-		users:function(){return this.$store.state.users}
+		users:function(){return this.$store.state.users},
+		teenAger:function(){return this.$store.getters.teenAger}
 	}
 }
 </script>
