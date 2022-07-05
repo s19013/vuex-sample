ed77cc7f3e14ea6b4a0d54acb992c314acbfb190
知念英弥 <s19013@std.it-college.ac.jp>
2022-07-05 12:03:33 +0900

    コンポーネント作成

---
+++ src/components/UserList.vue
@@ -0,0 +1,15 @@
+<template>
+  <ul>
+		<li v-for="user in users"  v-bind:key="user.name">
+			{{ user.name }} ({{ user.email }})
+		</li>
+  </ul>
+</template>
+
+<script>
+export default {
+computed:{
+		users:function(){return this.$store.state.users}
+	}
+}
+</script>
